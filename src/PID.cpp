28bfcd30a99b33082f3119f48d40c74c8aa92776
Aditya Makhija <134855421+adityacodes5@users.noreply.github.com>
2024-02-07 23:18:59 -0500

    Untested PID code. Needs tweaking

--- src/PID.cpp
+++ src/PID.cpp
@@ -43,10 +43,44 @@ double PID::compute(double error){
 
     timeSpentRunning += 10;
 
+    if (output > 100){
+        output = 100;
+    }
 
+    else if (output < -100){
+        output = -100;
+    }
 
     return (output);
 }
+
+
+void PID::setValues(double error = 0, double kP = 0, double kI = 0, double kD = 0, double startIntegral = 0, double settleError = 0, double settleTime = 0, double timeout = 0){
+
+    //Possibly set values
+    this-> error = error;
+    this-> kP = kP;
+    this-> kI = kI;
+    this-> kD = kD;
+    this-> startIntegral = startIntegral;
+    this-> settleError = settleError;
+    this-> settleTime = settleTime;
+    this-> timeout = timeout;
+    //Reset rest to 0
+    this-> accumulatedError = 0;
+    this-> previousError = 0;
+    this-> output = 0;
+    this-> timeSpentSettled = 0;
+    this-> timeSpentRunning = 0;
+    this-> degreesError = 0;
+    this-> motorSpeed = 0;
+    this-> moveEnabled = false;
+
+    resetDegreePosition();
+    resetHeading();
+    
+}
+
 
 bool PID::isSettled(){
 
@@ -54,7 +88,7 @@ bool PID::isSettled(){
         return true;
     }
 
-    else if(timeSpentRunning > timeout){
+    else if(timeSpentRunning > timeout && timeout != 0){ // If timeout does equal 0, the move will never actually time out. Setting timeout to 0 is the same as setting it to infinity
         return true;
     }
 
@@ -62,4 +96,41 @@ bool PID::isSettled(){
         return false;
     }
 
+}
+
+void PID::moveFor(double inches){
+    setValues(inches, 0, 0, 0, 2, 90, 250, 5000); //Please test and tune these values as required. Set P, I, and D values
+    moveEnabled = true;
+    while(moveEnabled){
+        
+        degreesError = ((inches * 360) / (wheelCircumference * gearRatio)) - calculateAverageMotorRotation();
+        motorSpeed = compute(degreesError);
+        move(fwd, motorSpeed, motorSpeed);
+
+        if (isSettled()){
+            moveEnabled = false;
+            break;
+        }
+
+        vexDelay(deltaTime);
+    }
+
+
+}
+
+void PID::turnFor(double degrees){
+    setValues(degrees, 0, 0, 0, 5, 15, 250, 5000); //Please test and tune these values as required. Set P, I, and D values
+    moveEnabled = true;
+    while(moveEnabled){
+        move(fwd, motorSpeed, -motorSpeed);
+
+        if (isSettled()){
+             degreesError = degrees - getHeading(); //Future note: try turning robot without resetting heading to see if it works
+            motorSpeed = compute(degreesError);
+            moveEnabled = false;
+            break;
+        }
+
+        vexDelay(deltaTime);
+    }
 }
